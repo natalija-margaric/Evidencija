3ac3116549d49f62e8d41d1f2d837502692c63f9
natalija-margaric <43842722+natalija-margaric@users.noreply.github.com>
2019-05-29 17:34:24 +0200

    Add files via upload

---
+++ Evidencija_app/nat.py
@@ -0,0 +1,55 @@
+
+from flask import Flask, Response, jsonify, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Student (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+         return f"Student('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Profesor (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)   
+
+    def __repr__(self):
+         return f"Profesor('{self.username}', '{self.email}', '{self.image_file}')" 
+
+    class Kolegij (db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        Naziv = db.Column(db.String(20))   
+        
+
+    class Profil (db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        Ime: db.Column(db.String(30))
+        Prezime: db.Column(db.String(30))
+        Profesor_id = db.Column(db.Integer)
+        Student_jmbag = db.Column(db.Integer)
+
+    class Prisutnost (db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        Ucenici_jmbag = db.Column (db.Integer)
+        Naziv_kolegija = db.Column (db.String(20))
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('layout.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
