5be45c505d4ecb6db68924ec559d440b902d0b53
natalija-margaric <43842722+natalija-margaric@users.noreply.github.com>
2019-05-30 22:33:44 +0200

    Add files via upload

--- Evidencija_app/nat.py
+++ Evidencija_app/nat.py
@@ -22,14 +22,29 @@ class Profesor (db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)   
+    password = db.Column(db.String(60), nullable=False) 
+
+    profesori= db.relationship('kolegij', backref='profesor', lazy='dynamic')
+    profili=db.relationship('profil', backref='profesor', uselist=False)  
 
     def __repr__(self):
          return f"Profesor('{self.username}', '{self.email}', '{self.image_file}')" 
 
+
     class Kolegij (db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        Naziv = db.Column(db.String(20))   
+        Naziv = db.Column(db.String(20))
+
+        profesor_id= db.Column(db.Integer, db.ForeignKey('profesor.id'))
+        prisutnosti=db.relationship('prisutnost', backref='kolegij', lazy='dynamic')
+
+        def __repr__(self):
+         return f"kolegij('{self.naziv})" 
+
+    db.Table('profil_kolegij',)
+    db.Column('profil_id', db.Integer, db.ForeignKey('profil.id')),
+    db.Column('kolegij_id', db.Integer, db.ForeignKey('kolegij.id'))
+   
         
 
     class Profil (db.Model):
@@ -38,6 +53,13 @@ class Profesor (db.Model):
         Prezime: db.Column(db.String(30))
         Profesor_id = db.Column(db.Integer)
         Student_jmbag = db.Column(db.Integer)
+
+        prof_id = db.Column(db.Integer, db.ForeignKey('profesor.id'), unique=True)
+stud_id= db.Column(db.Integer, db.ForeignKey ('student.id'), unique=True)
+kolegiji=db.relationship ('kolegij', secondary='profil_kolegij', backref='profil', lazy='dynamic')
+
+def __repr__(self):
+         return f"profil('{self.ime}', '{self.prezime}')"
 
     class Prisutnost (db.Model):
         id = db.Column(db.Integer, primary_key=True)
